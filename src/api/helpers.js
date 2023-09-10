43d280d7aabc9187a75ba05ec7d4ee54d061e192
Eugenia Herrera <eugenia.herrera@wilko.com>
2023-09-10 11:43:26 +0100

    refactored css

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -5,6 +5,6 @@
  * @return {Promise<Object>}
  */
 export async function request(apiUrl) {
-  console.log(apiUrl)
+  console.log(apiUrl);
   return apiUrl;
 }
--- src/api/index.js
+++ src/api/index.js
@@ -33,7 +33,7 @@ export default async function getData() {
       })
 
   ));
-  request(arr)
+  request(arr);
   await axios.get(arr);
   // console.log(arr);
   return arr;
