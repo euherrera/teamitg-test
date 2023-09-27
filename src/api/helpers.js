f106d30b2f5b34d518d0ce9e822e1a0c096e3cbf
euherrera <eu.herrera@gmail.com>
2023-09-27 20:55:16 +0100

    refactored api call

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -4,8 +4,36 @@
  * @param {string} apiUrl
  * @return {Promise<Object>}
  */
-export async function request(data, arr) {
-  // console.log(arr);
+import axios from 'axios';
+export async function request() {
+  const res = await axios.get('/api/vehicles.json/');
+  const { data } = res;
 
+  const url = Object.values(data).map((value) => (
+    value.apiUrl
+  ));
+
+  let price;
+  const arr = [];
+  Object.entries(url).map(([key, value]) => (
+
+    axios.get(value)
+      .then((resp) => {
+        price = (resp.data.price && resp.data.price !== undefined) ? resp.data.price : '';
+        if (price) {
+          const { meta, id, description } = resp.data;
+          data[key].price = price;
+          data[key].meta = meta;
+          data[key].id = id;
+          data[key].description = description;
+          arr.push(data[key]);
+        }
+        return price;
+      })
+      .catch(() => {
+      })
+
+  ));
+  await axios.get(arr);
   return arr;
 }
--- src/api/index.js
+++ src/api/index.js
@@ -9,37 +9,5 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  const res = await axios.get('/api/vehicles.json/');
-  const { data } = res;
-
-  const url = Object.values(data).map((value) => (
-    value.apiUrl
-  ));
-
-  let price;
-  const arr = [];
-  Object.entries(url).map(([key, value]) => (
-
-    axios.get(value)
-      .then((resp) => {
-        price = (resp.data.price && resp.data.price !== undefined) ? resp.data.price : '';
-        if (price) {
-          const { meta, id, description } = resp.data;
-          data[key].price = price;
-          data[key].meta = meta;
-          data[key].id = id;
-          data[key].description = description;
-          arr.push(data[key]);
-        }
-        return price;
-      })
-      .catch(() => {
-        // console.error(e.message);
-      })
-
-  ));
-  request(arr);
-  await axios.get(arr);
-  // console.log(arr);
-  return arr;
+  return request();
 }
