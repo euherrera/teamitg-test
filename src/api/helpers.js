662a65956f67ee2864804e3f0f385552bc8ea951
euherrera <eu.herrera@gmail.com>
2023-10-02 18:56:39 +0100

    reworked

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -6,36 +6,45 @@
  */
 import axios from 'axios';
 export async function request() {
- let res;
+
  let arr = [];
- try {
-  res = await axios.get('/api/vehicles.json');
-  const { data } = res;
-  console.log(data)
-  try {
-    Object.entries(data).map(async([key, value])  => (
-       res = await axios.get(value.apiUrl),
+ let res;
+ async function getUserData() {
+  let resp = await axios.get('/api/vehicles.json');
+  const {data}  = resp;
+  return data
+}
+
+async function cleanUserData(userData) {
+  console.log(userData)
+  Object.entries(userData).map(async([key, value])  => (
+    res = await axios.get(value.apiUrl),
+    userData[key].price = res.data.price,
+    userData[key].meta = res.data.meta,
+    userData[key].id = res.data.id,
+    userData[key].description = res.data.description,
+    (res.data.price  && res.data.price !== undefined) ? arr.push(userData[key]) : ''
+    
+  ))
   
-       data[key].price = res.data.price,
-       data[key].meta = res.data.meta,
-       data[key].id = res.data.id,
-       data[key].description = res.data.description,
-       (res.data.price  && res.data.price !== undefined) ? arr.push(data[key]) : ''
-      
-     ))
-    }catch(error){
-      console.log(error)
-    }
- } catch(error){
-  console.log(error)
- }
- 
-   try {
-    await axios.get(arr);
-    console.log(arr.length)
-    return  arr;
-   } catch(error){
-      console.log(error)
-   }
+  return arr;
+  
+
+}
+async function saveToDataBase(userData) {
+  console.log('Saving to DB'); 
+    await axios.get(userData);
+    console.log(userData)
+    return  userData;
+   
+}
+
+async function cleanAndSaveUserData() {
+  const userData = await getUserData();
+  const cleanedData = await cleanUserData(userData);
+  console.log('cleaned data',cleanedData)
+  return await saveToDataBase(cleanedData);
+}
+return cleanAndSaveUserData(); // does all the work
   
 }
